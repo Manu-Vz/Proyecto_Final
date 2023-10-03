239363c749dcc987f9412cf56e54293dc580828a
Manuel Vasquez <manuvasquez2004@gmail.com>
2023-10-03 12:17:11 -0300

    Primer Commit

---
+++ src/Main/Proyecto_Final.java
@@ -0,0 +1,15 @@
+package Main;
+
+import AccesoDatos.Conectar;
+
+public class Proyecto_Final {
+
+    public static void main(String[] args) {
+        Conectar.getConectar();
+
+        
+        System.out.println("Final");
+        
+    }
+    
+}
