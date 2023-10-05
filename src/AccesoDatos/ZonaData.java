a988bbd42ca1368ef4845744a862d791407f5c80
Manuel Vasquez <manuvasquez2004@gmail.com>
2023-10-05 10:52:10 -0300

    Zona y ZonaData completadas

--- src/AccesoDatos/ZonaData.java
+++ src/AccesoDatos/ZonaData.java
@@ -1,5 +1,15 @@
 package AccesoDatos;
 
+import Entidades.Zona;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 import org.mariadb.jdbc.Connection;
 
 /**
@@ -12,6 +22,113 @@ public class ZonaData {
     
     public ZonaData(){
         con = Conectar.getConectar();
+    }
+    
+    public void agregarZona (Zona zona){
+        String sql = "INSERT INTO zona(nombre, descripcion, estado) VALUES (?, ?, 1)";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            ps.setString(1, zona.getNombre());
+            ps.setString(2, zona.getDescripcion());
+            
+            ps.executeUpdate();
+            
+            ResultSet rs = ps.getGeneratedKeys();
+            if(rs.next()){
+                zona.setIdZona(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Se ha agregado una nueva Zona correctamente");
+            }
+            
+            ps.close();
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al agregar zona "+ ex.getMessage());
+        }
+        
+    }
+    
+    public void modificarZona (Zona zona){
+        String sql = "UPDATE zona SET nombre = ?, descripcion = ?,"
+                + " estado = ? WHERE idZona = ?";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            ps.setString(1, zona.getNombre());
+            ps.setString(2, zona.getDescripcion());
+            ps.setBoolean(3, zona.isEstado());
+            ps.setInt(4, zona.getIdZona());
+            
+            int rs = ps.executeUpdate();
+            
+            if(rs > 0){
+                JOptionPane.showMessageDialog(null, "La Zona se ha modificado correctamente");
+            }
+            
+            ps.close();
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al actualizar zona "+ ex.getMessage());
+        }
+        
+        
+    }
+    
+    public List<Zona> listarZonas (){
+        List<Zona> listaZona = new ArrayList();
+        Zona zona;
+        String sql = "SELECT * FROM zona WHERE estado = 1";
+        
+        try{
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            
+            while(rs.next()){
+                zona = new Zona();
+                zona.setIdZona(rs.getInt("idZona"));
+                zona.setNombre(rs.getString("nombre"));
+                zona.setDescripcion(rs.getString("descripcion"));
+                zona.setEstado(rs.getBoolean("estado"));
+                listaZona.add(zona);
+            }
+            
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,  "Error al consultar la base de datos "+ex.getMessage());
+        }
+        
+        return listaZona;
+    
+    }
+    
+    public Zona buscarZona (int id){
+        Zona zona = new Zona();
+        String sql = "SELECT * FROM zona WHERE idZona = ?";
+        PreparedStatement ps;
+        
+        try {
+            
+            ps = con.prepareStatement(sql);
+            
+            ps.setInt(1, id);
+            
+            ResultSet rs = ps.executeQuery();
+            
+            if(rs.next()){
+                zona.setIdZona(rs.getInt("idZona"));
+                zona.setNombre(rs.getString("nombre"));
+                zona.setDescripcion(rs.getString("descripcion"));
+                zona.setEstado(rs.getBoolean("estado"));
+            }
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,  "Error al consultar la base de datos "+ex.getMessage());
+        }
+        
+        return zona;
     }
     
 }
--- src/Entidades/Zona.java
+++ src/Entidades/Zona.java
@@ -9,5 +9,62 @@ package Entidades;
  * @author kalema
  */
 public class Zona {
+    private int idZona;
+    private String nombre;
+    private String descripcion;
+    private boolean estado;
+    
+    public Zona (){}
+
+    public Zona(String nombre, String descripcion, boolean estado) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.estado = estado;
+    }
+
+    public Zona(int idZona, String nombre, String descripcion, boolean estado) {
+        this.idZona = idZona;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.estado = estado;
+    }
+
+    public int getIdZona() {
+        return idZona;
+    }
+
+    public void setIdZona(int idZona) {
+        this.idZona = idZona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Zona{" + "idZona=" + idZona + ", nombre=" + nombre + ", descripcion=" + descripcion + ", estado=" + estado + '}';
+    }
+    
     
 }
