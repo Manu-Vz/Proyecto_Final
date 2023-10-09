84f5410960fe94d99b58c5e3cb28eadcf75573e3
Manuel Vasquez <manuvasquez2004@gmail.com>
2023-10-09 10:50:30 -0300

    Update MenuPrincipal.java

--- src/Vistas/MenuPrincipal.java
+++ src/Vistas/MenuPrincipal.java
@@ -32,6 +32,8 @@ public class MenuPrincipal extends javax.swing.JFrame {
         escritorio = new javax.swing.JDesktopPane();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
         jMenuItemPropiedades = new javax.swing.JMenuItem();
 
@@ -52,6 +54,18 @@ public class MenuPrincipal extends javax.swing.JFrame {
         );
 
         jMenu1.setText("Inquilinos");
+
+        jMenuItem1.setText("Ver Propiedades");
+        jMenu1.add(jMenuItem1);
+
+        jMenuItem2.setText("Ver Contratos");
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem2);
+
         jMenuBar1.add(jMenu1);
 
         jMenu2.setText("Propiedades");
@@ -90,6 +104,10 @@ public class MenuPrincipal extends javax.swing.JFrame {
         vistaProp.setVisible(true);
         escritorio.add(vistaProp);
     }//GEN-LAST:event_jMenuItemPropiedadesActionPerformed
+
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -131,6 +149,8 @@ public class MenuPrincipal extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JMenuItem jMenuItemPropiedades;
     private javax.swing.JRadioButtonMenuItem jRadioButtonMenuItem1;
     // End of variables declaration//GEN-END:variables
