7f1b14831edd044f3b27e6a64991cd4896b73fbf
Manuel Vasquez <manuvasquez2004@gmail.com>
2023-10-12 11:56:54 -0300

    detallitos Agregados a PropiedadesInquilinos

--- src/Vistas/PropiedadesInquilinos.java
+++ src/Vistas/PropiedadesInquilinos.java
@@ -1,7 +1,9 @@
 package Vistas;
 
+import AccesoDatos.PropiedadData;
 import AccesoDatos.TipoLocalData;
 import AccesoDatos.ZonaData;
+import Entidades.PropiedadInmueble;
 import Entidades.TipoLocal;
 import Entidades.Zona;
 import java.util.ArrayList;
@@ -56,10 +58,18 @@ public class PropiedadesInquilinos extends javax.swing.JInternalFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         tabla = new javax.swing.JTable();
         jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+
+        setClosable(true);
 
         jPanel1.setBackground(new java.awt.Color(204, 204, 255));
 
         ComboBoxZonas.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        ComboBoxZonas.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                ComboBoxZonasFocusLost(evt);
+            }
+        });
         ComboBoxZonas.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 ComboBoxZonasActionPerformed(evt);
@@ -111,18 +121,28 @@ public class PropiedadesInquilinos extends javax.swing.JInternalFrame {
 
         jButton1.setText("Alquilar Propiedad");
 
+        jButton2.setText("Borrar Filtros");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 700, Short.MAX_VALUE)
-                .addContainerGap())
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(183, 183, 183)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 190, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 700, Short.MAX_VALUE))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(245, 245, 245)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 190, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jButton2)))
+                .addContainerGap())
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -130,8 +150,10 @@ public class PropiedadesInquilinos extends javax.swing.JInternalFrame {
                 .addGap(12, 12, 12)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 324, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(20, Short.MAX_VALUE))
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton2))
+                .addContainerGap(22, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
@@ -202,12 +224,25 @@ public class PropiedadesInquilinos extends javax.swing.JInternalFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_ComboBoxPrecioActionPerformed
 
+    private void ComboBoxZonasFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_ComboBoxZonasFocusLost
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_ComboBoxZonasFocusLost
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        ComboBoxLocal.setSelectedIndex(0);
+        ComboBoxPrecio.setSelectedIndex(0);
+        ComboBoxZonas.setSelectedIndex(0);
+    }//GEN-LAST:event_jButton2ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JComboBox<String> ComboBoxLocal;
     private javax.swing.JComboBox<String> ComboBoxPrecio;
     private javax.swing.JComboBox<String> ComboBoxZonas;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
@@ -239,6 +274,7 @@ public class PropiedadesInquilinos extends javax.swing.JInternalFrame {
     }
     
     private void armarComboBoxPrecios(){
+        modeloComboBoxPrecio.addElement("Seleccione una opcion");
         modeloComboBoxPrecio.addElement("Desde 0$ hasta 100.000$");
         modeloComboBoxPrecio.addElement("Desde 100.000$ hasta 150.000$");
         modeloComboBoxPrecio.addElement("Desde 150.000$ hasta 200.000$");
@@ -253,6 +289,16 @@ public class PropiedadesInquilinos extends javax.swing.JInternalFrame {
         modeloTabla.addColumn("Zona");
         modeloTabla.addColumn("Estado del Local");
         modeloTabla.addColumn("Precio");
+        
+        PropiedadData pd = new PropiedadData();
+        List<PropiedadInmueble> listPi = new ArrayList();
+        listPi = pd.buscarPropiedad();
+        
+        for(PropiedadInmueble p : listPi){
+            modeloTabla.addRow(new Object[]{p.getTipoLocal().getNombre(), 
+                p.getDireccion(), p.getZona().getNombre(),
+                p.getEstadoLocal().getNombre(), p.getPrecioTrazado() });
+        }
         tabla.setModel(modeloTabla);
     }
 
